72c510c16853f7fee132b22d060838653e27f613
Bereno <burracha.6464@gmail.com>
2024-09-11 23:07:16 -0300

    adiciona as bases do TAD

---
+++ TAD Aluno/aluno.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include "aluno.h" //para poder usar a classe aluno
+
+using namespace std;
---
+++ TAD Aluno/aluno.h
@@ -0,0 +1,12 @@
+#include <string>
+using namespace std;
+// include para usar o tipo string + std para evitar usar std ::
+
+class Aluno
+{
+    private:
+    //tudo que está aqui só pode ser acessado pela classe
+
+    public:
+    //tudo que está aqui pode ser acessado por um arquivo com o include aluno.h
+};
