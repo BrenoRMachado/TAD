2250be0b36f18a781efc9bd53ae9936553fe57fa
Bereno <burracha.6464@gmail.com>
2024-09-14 15:32:20 -0300

    Finalização da primeira etapa

--- TAD Aluno/main.cpp
+++ TAD Aluno/main.cpp
@@ -12,10 +12,12 @@ int main ()
     // (variáveis 1)
     string n = "Nome" , mat = "Matricula";
     //
-
+    // (variáveis 2)
+    int age = 0;
+    //
     // ------------------------------------------------------------------------------
 
-    // Criação don aluno como objeto ------------------------------------------------
+    // Criação do aluno como objeto ------------------------------------------------
 
     Aluno breno(n,mat);
 
@@ -23,8 +25,31 @@ int main ()
 
     // aplicação --------------------------------------------------------------------
 
+    //imprimir todas as informações do Aluno 
+    // (Definiçaõ do Aluno)
 
+    cout << "Defina o nome : ";
+    cin >> n;
+    breno.setNome(n);
 
+    cout << "Defina a matricula : ";
+    cin >> mat;
+    breno.setMatricula(mat);
+
+    cout << "Defina a idade : ";
+    cin >> age;
+    breno.setIdade(age);
+
+    cout << "Digite as notas do aluno: " <<endl; 
+    breno.leNotas();
+
+    // (impressão)
+
+    cout << "As informacoes do aluno sao: " <<endl;
+    cout << "Nome: " << breno.getNome() <<endl;
+    cout << "Matricula: "<< breno.getMatricula() <<endl;
+    cout << "Idade: " << breno.getIdade() <<endl;
+    cout << "Media: " << breno.calculaMedia() <<endl;
 
 
     
