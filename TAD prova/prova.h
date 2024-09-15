a11da113fba68730fbfa5df90608b8d9f333196f
Bereno <burracha.6464@gmail.com>
2024-09-14 21:53:32 -0300

    adiciona estrutura da classe

--- TAD prova/prova.h
+++ TAD prova/prova.h
@@ -2,6 +2,33 @@ class Prova
 {
     private:
 
+    // Todas as variáveis que eu irei usar -----------------------------------------
+    int n; //numero de questões
+    double *notasDasQuestoes; //vetor dinâmico que vai alocar a nota de cada questão
+    double notaFinal; // variavel que vai armazenar o valor final
+    // -----------------------------------------------------------------------------
+
     public:
+
+    // construtor e destrutor ------------------------------------------------------
+    //são declarados sem tipo, apenas o nome da classe
+
+    Prova( int numero );
+    ~Prova ();
+
+    // -----------------------------------------------------------------------------
+
+    // metodos ---------------------------------------------------------------------
+    // as funções são declaradas de forma padrão, mas sua implementação é feita no 
+    // arquivo.cpp 
+    
+    void leNotaQuestao(); //função que vai ler as notas de cada questão
+
+    void calculaNota(); //função que vai calcular a nota final
+
+    double getNotaFinal(); //função que vai mostrar a nota final
+
+    // -----------------------------------------------------------------------------
+    
 
 };
