d49717f0d6028f134d3db605d4ef6ebdab30831f
Bereno <burracha.6464@gmail.com>
2024-09-14 22:15:16 -0300

    adiciona implementações

--- TAD prova/main.cpp
+++ TAD prova/main.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main ()
 {
+    int numero; // vai servir de equivalente a numero de questões aqui no main
+
+    Prova prova1(numero); //Precisa de parenteses por que o construtor possui parâmetros
+
+    cout << "Digite as notas do aluno: \n";
+    prova1.leNotaQuestao(); // chamou para ler cada nota do aluno
+
+    prova1.calculaNota(); // chamou para calcular a nota final 
+
+    cout << "A nota final eh: " << prova1.getNotaFinal();
+    // chamou para mostrar a nota final
+
+
 
     return 0;
 }
--- TAD prova/prova.cpp
+++ TAD prova/prova.cpp
@@ -6,28 +6,52 @@ using namespace std;
 // construtor e destrutor --------------------------------------------------------
 Prova :: Prova(int numero)
 {
-
+    n = numero; // associa o correspondendo numero da main ao n da classe
+    notasDasQuestoes = new double[n]; //cria o vetor dinamicamente 
 }
 
 Prova :: ~Prova()
 {
-
+    delete [] notasDasQuestoes; //limpa a memória do vetor dinâmico
 }
 // ------------------------------------------------------------------------------
 
 // metodos ----------------------------------------------------------------------
 void Prova :: leNotaQuestao()
 {
-
+    for ( int i = 0 ; i < n ; i++)
+    {
+        cout << "O aluno tirou quanto na questao " << i+1 << " ?"<<endl;
+        cin >> notasDasQuestoes[i];
+    }
 }
 
 void Prova :: calculaNota()
 {
+    // descobrir qual a menor
+    int menor = notasDasQuestoes[0];
+    for ( int i = 1 ; i < n ; i++)
+    {
+        if ( notasDasQuestoes[i] < menor)
+        {
+            menor = notasDasQuestoes[i];
+        }
+    }
+    //
+    cout << "A nota final eh: ";
+    
+    for ( int i = 0 ; i<n ; i++)
+    {
+        if ( notasDasQuestoes[i] != menor)
+        {
+            notaFinal += notasDasQuestoes[i];
+        }
+    }
 
 }
 
 double Prova :: getNotaFinal()
 {
-
+    return notaFinal;
 }
 // ------------------------------------------------------------------------------
